3ef43079ca09e66363bb23791b764e6584b96d93
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-12-28 13:03:19 +0300

    Developed Result view

--- TipBuddy/Views/TBResultView.swift
+++ TipBuddy/Views/TBResultView.swift
@@ -9,6 +9,40 @@ import UIKit
 import SnapKit
 
 class TBResultView: UIView {
+    
+    private let headerLabel = TBLabel(text: "Total p/person", font: ThemeFont.demibold(ofSize: 18))
+    
+    private let amountPerPersonLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        let text = NSMutableAttributedString(string: "$0", attributes: [.font: ThemeFont.bold(ofSize: 48)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let horizontalLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ThemeColor.separatorColor
+        return view
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [headerLabel, amountPerPersonLabel, horizontalLineView, horizontalStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    private let amountViewOne = TBAmountView()
+    private let amountViewTwo = TBAmountView()
+    
+    private lazy var horizontalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [amountViewOne, UIView(), amountViewTwo])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -20,6 +54,20 @@ class TBResultView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .systemRed
+        backgroundColor = .white
+        addSubview(verticalStackView)
+        
+        verticalStackView.snp.makeConstraints { make in
+            make.top.equalTo(snp.top).offset(24)
+            make.left.equalTo(snp.left).offset(24)
+            make.right.equalTo(snp.right).offset(-24)
+            make.bottom.equalTo(snp.bottom).offset(-24)
+        }
+        
+        horizontalLineView.snp.makeConstraints { make in
+            make.height.equalTo(2)
+        }
+        
+        addShadow(offset: CGSize(width: 0, height: 3), color: .black, radius: 12, opacity: 0.1)
     }
 }
