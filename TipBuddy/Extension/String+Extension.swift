4f0c7f717eb78234bc343fe841ac8d9aca20bf49
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-12-30 15:44:49 +0300

    Created string extension

---
+++ TipBuddy/Extension/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  TipBuddy
+//
+//  Created by Celil Çağatay Gedik on 30.12.2023.
+//
+
+import Foundation
+
+extension String {
+    var doubleValue: Double? {
+        Double(self)
+    }
+}
