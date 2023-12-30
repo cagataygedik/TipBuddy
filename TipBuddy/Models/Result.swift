41b2939818db8a3b237398d01282257649f63bf8
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-12-30 15:44:36 +0300

    Created Result model

---
+++ TipBuddy/Models/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  TipBuddy
+//
+//  Created by Celil Çağatay Gedik on 30.12.2023.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
